4e4cca9fce066cc7b3f7e6a2c6c4a6112ec637cf
Hirak-Debadhikary <103572541+Hirak-Debadhikary@users.noreply.github.com>
2022-12-14 21:24:41 +0530

    Day 1  fp04_462

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,10 @@
-
+import WorkSpace from "./component/WorkSpace";
+// import Test from "./component/Test";
 function App() {
   return (
     <div className="App">
+      <WorkSpace />
+      {/* <Test /> */}
     </div>
   );
 }
