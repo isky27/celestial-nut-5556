d0caa3408c9ba056b57149e4692d9371832180c5
Ghanishtkhurana <101393463+Ghanishtkhurana@users.noreply.github.com>
2022-12-15 21:22:09 +0530

    day3

--- frontend/src/pages/AllRoutes.jsx
+++ frontend/src/pages/AllRoutes.jsx
@@ -11,7 +11,7 @@ const AllRoutes = () => {
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login/>} />
       <Route path="/signup" element={<Signup/>} />
-      <Route path="/marketplace" element={<Marketplace/>} />
+      
     </Routes>
   );
 }
