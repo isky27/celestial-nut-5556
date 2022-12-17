de725ed69542a484a9deaa15f618f7c2ce4e382e
Divyanka Rai <raidivyanka40@gmail.com>
2022-12-17 21:52:34 +0530

    d5

--- frontend/src/pages/Interface-designer.jsx
+++ frontend/src/pages/Interface-designer.jsx
@@ -7,108 +7,289 @@ SimpleGrid,
 AccordionPanel,
 AccordionButton,
 AccordionItem,
-Accordion
+Accordion,
+Stack,
+Heading,
+Container,Flex
 } 
 from '@chakra-ui/react'
 
 const Interfacedesigner = () => {
   return (
     <>
-        <Box bg='#1a3866' w='100%' p={40} color='white'>
-            <Text fontSize='6xl' marginTop='-12%' textAlign='center' fontWeight='bold' color='gray300'>
+        <Box bg='#1a3866' width={[
+            '150%', // 0-30em
+            '100%', // 30em-48em
+            '100%', // 48em-62em
+            '100%', // 62em+
+            ]} p={8} textAlign='center' color='white'>
+            <Text fontSize={['md', '2xl', '4xl', '6xl']} marginTop='-2%' ml={[0, 0, 0, 20]} fontWeight='bold' color='gray300'>
                 Reimagine your workflows with Interface Designer
             </Text>
-            <Text fontSize='xl'  textAlign='center' fontWeight='bold' marginTop='2' color='gray300'>
+            <Text fontSize={['sm', 'md', 'xl', 'xl']} fontWeight='bold' marginTop='2' color='gray300'>
                 Keep teams connected and working together with custom interfaces.
             </Text>
-            <Button colorScheme='blue' marginLeft='96' marginTop='6' fontWeight='bold' size='lg'>Get Started For Free</Button>
-            <Image height='400px' width='8xl' marginTop='16' marginBottom='-40' src='https://images.ctfassets.net/wl95ljfippl8/66whWGlDV24fjLHmfIvgzw/0f2911110b141203aae07c0412dc45e3/Tier1-InterfaceDesigner-01-21x9.png?w=1920' alt='Dan Abramov' />
+            <Button colorScheme='blue' ml={[4, 10, 94,20]} marginTop='6' fontWeight='bold' size='lg'>Get Started For Free</Button>
         </Box>
-        <Box w='100%' display='flex' marginTop='8'>
+        <Box bg='#1a3866' width={[
+            '150%', 
+            '100%', 
+            '100%', 
+            '100%', 
+            ]} textAlign='center' color='white'>
+            <Image height={[
+            '150%', 
+            '100%', 
+            '320',
+            '360', 
+            ]} width='7xl' marginTop='-4'  src='https://images.ctfassets.net/wl95ljfippl8/66whWGlDV24fjLHmfIvgzw/0f2911110b141203aae07c0412dc45e3/Tier1-InterfaceDesigner-01-21x9.png?w=1920' alt='Dan Abramov' />
+        </Box>
+        <Box w='100%' display='flex' marginTop='6'>
             <Box p='4' width='60%'>
-                    <Text fontSize='4xl' marginLeft='32' fontWeight='bold' color='black'>
+                    <Text fontSize={['md', '2xl', '3xl', '4xl']} ml={[0, 0, 0, 32]} fontWeight='bold' color='black'>
                         The power to create is in your hands
                     </Text>
                 </Box>
                 <Box p='4' width='40%'>
-                    <Text fontSize='xl' marginRight='32' color='black'>
+                    <Text fontSize={['sm', 'md', 'md', 'xl']} mr={[-32, -20, 0, 32]} color='black'>
                     Interface Designer enables anyone, at any technical skill level, to build and share custom interfaces without code.
                     </Text>
                 </Box>
         </Box>
-        <Box width='100%' p={10}>
-            <Image width='100%' height='96' src='https://images.ctfassets.net/wl95ljfippl8/qrBqADBtmq6eQ8pPACMwl/996c3db331ea49b36359823c9bb498e0/Tier1-InterfaceDesigner-01.2-21x9.png?w=1920' alt='Dan Abramov' />
+        <Box width={[
+            '150%', 
+            '100%', 
+            '100%', 
+            '100%', 
+            ]} textAlign='center' color='white'>
+            <Image height={[
+            '150%', 
+            '100%', 
+            '320',
+            '360', 
+            ]} width='8xl' marginTop='4'  src='https://images.ctfassets.net/wl95ljfippl8/qrBqADBtmq6eQ8pPACMwl/996c3db331ea49b36359823c9bb498e0/Tier1-InterfaceDesigner-01.2-21x9.png?w=1920' alt='Dan Abramov' />
         </Box>
         {/* ********************** box 1 ******************* */}
-        <Box height='96' w='100%' display='flex' marginTop='8'>
-                <Box p='4' height='100%' width='58%'>
-                    <Image borderRadius='12' width='67%' height='99%' margin='auto' src='https://images.ctfassets.net/wl95ljfippl8/5decmcG06ZzDqIfXE5OR9m/d9eb2a6460a4c97b3a010839bd419b0d/Tier1-InterfaceDesigner-02.png?w=1920' alt='Dan Abramov' />
-                </Box>
-                <Box p='4' width='42%'>
-                    <Text fontSize='4xl' marginTop='14' fontWeight='bold' marginRight='44' color='black'>
-                    Create your first interface in minutes
-                    </Text>
-                    <Text fontSize='xl' marginRight='28' color='gray'>
-                    Drag-and-drop components make it simple to design completely custom interfaces right away.
-                    </Text>
-                </Box>
-        </Box>
+         <Container maxW={"7xl"}>
+          <Stack
+            align={"center"}
+            spacing={{ base: 8, md: 10 }}
+            py={{ base: 20, md: 28 }}
+            direction={{ base: "column", md: "row" }}
+          >
+            <Flex
+              flex={1}
+              justify={"center"}
+              align={"center"}
+              position={"relative"}
+              w={"full"}
+            >
+              <Box
+                position={"relative"}
+                rounded={"2xl"}
+                width={"full"}
+              >
+                <Image
+                  alt={"Hero Image"}
+                  fit={"cover"}
+                  align={"center"}
+                  w={"80%"}
+                  borderRadius={'24'}
+                  ml='20'
+                  h={"100%"}
+                  src={
+                    "https://images.ctfassets.net/wl95ljfippl8/6gJGMD4X15k2H8bbxaNgx8/6316c79d05fffbdbf174292f7c5733dd/Tier1-InterfaceDesigner-04.png?w=1920"
+                  }
+                />
+              </Box>
+            </Flex>
+            <Stack flex={1} spacing={{ base: 5, md: 10 }}>
+              <Stack spacing={"2"} w={"80%"} ml='16'>
+                <Heading
+                  lineHeight={1.1}
+                  fontWeight={700}
+                  mr='4'
+                  fontSize={{ base: "3xl", sm: "4xl", lg: "4xl" }}
+                  >
+                    Provide the right info to the right people
+                </Heading>
+                <Text fontSize={'xl'} color={"gray.500"}>
+                Some teammates don't need access to your full workflow. Permissions let you decide who sees what. 
+                </Text>
+              </Stack>
+            </Stack>
+          </Stack>
+        </Container>
         {/* ********************** box 2 ******************* */}
-        <Box height='96' w='100%' display='flex' marginTop='8'>
-                <Box p='4' height='100%'  width='58%'>
-                    <Text fontSize='4xl' marginLeft='28' marginTop='14' fontWeight='bold' marginRight='60' color='black'>
-                        Customize for anyone
-                    </Text>
-                    <Text mt='2' fontSize='xl' ml='28' marginRight='52' color='gray'>
+               <Container mt='-12' maxW={"7xl"}>
+          <Stack
+            align={"center"}
+            spacing={{ base: 8, md: 10 }}
+            py={{ base: 20, md: 28 }}
+            direction={{ base: "column", md: "row" }}
+          >
+            <Stack flex={1} spacing={{ base: 5, md: 10 }}>
+              <Stack spacing={"2"} ml='20' w={"80%"}>
+                <Heading
+                  lineHeight={1.1}
+                  fontWeight={700}
+                  fontSize={{ base: "3xl", sm: "4xl", lg: "4xl" }}
+                >
+                     Customize for anyone
+                </Heading>
+                <Text  mt='2' fontSize='xl' color={"gray.500"}>
                     Create interfaces that match the way each person on your team works, no matter what they are working on.
                     </Text>
-                </Box>
-                <Box p='4' width='42%'>
-                    <Image borderRadius='12' width='99%' height='100%' margin='auto' src='https://images.ctfassets.net/wl95ljfippl8/5decmcG06ZzDqIfXE5OR9m/d9eb2a6460a4c97b3a010839bd419b0d/Tier1-InterfaceDesigner-02.png?w=1920' alt='Dan Abramov' />
-                </Box>
-        </Box>
+              </Stack>
+            </Stack>
+            <Flex
+              flex={1}
+              justify={"center"}
+              align={"center"}
+              position={"relative"}
+              w={"full"}
+            >
+              <Box
+                position={"relative"}
+                rounded={"2xl"}
+                width={"full"}
+              >
+                <Image
+                  alt={"Hero Image"}
+                  fit={"cover"}
+                  align={"center"}
+                  borderRadius={'24'}
+                  w={"80%"}
+                  ml='20'
+                  h={"100%"}
+                  src={
+                    "https://images.ctfassets.net/wl95ljfippl8/5decmcG06ZzDqIfXE5OR9m/d9eb2a6460a4c97b3a010839bd419b0d/Tier1-InterfaceDesigner-02.png?w=1920"
+                  }
+                />
+              </Box>
+            </Flex>
+          </Stack>
+        </Container>
         {/* ********************** box 3 ******************* */}
-        <Box height='96' w='100%' display='flex' marginTop='8'>
-                <Box p='4' height='100%'  width='58%'>
-                    <Image borderRadius='12' width='67%' height='99%' margin='auto' src='https://images.ctfassets.net/wl95ljfippl8/6gJGMD4X15k2H8bbxaNgx8/6316c79d05fffbdbf174292f7c5733dd/Tier1-InterfaceDesigner-04.png?w=1920' alt='Dan Abramov' />
-                </Box>
-                <Box p='4' width='42%'>
-                    <Text fontSize='4xl' marginTop='14' fontWeight='bold' marginRight='44' color='black'>
+         <Container maxW={"7xl"} mt='-12'>
+          <Stack
+            align={"center"}
+            spacing={{ base: 8, md: 10 }}
+            py={{ base: 20, md: 28 }}
+            direction={{ base: "column", md: "row" }}
+          >
+            <Flex
+              flex={1}
+              justify={"center"}
+              align={"center"}
+              position={"relative"}
+              w={"full"}
+            >
+              <Box
+                position={"relative"}
+                rounded={"2xl"}
+                width={"full"}
+              >
+                <Image
+                  alt={"Hero Image"}
+                  fit={"cover"}
+                  align={"center"}
+                  w={"80%"}
+                  borderRadius={'24'}
+                  ml='20'
+                  h={"100%"}
+                  src={
+                    "https://images.ctfassets.net/wl95ljfippl8/6gJGMD4X15k2H8bbxaNgx8/6316c79d05fffbdbf174292f7c5733dd/Tier1-InterfaceDesigner-04.png?w=1920"
+                  }
+                />
+              </Box>
+            </Flex>
+            <Stack flex={1} spacing={{ base: 5, md: 10 }}>
+              <Stack spacing={"2"} w={"80%"} ml='16'>
+                <Heading
+                  lineHeight={1.1}
+                  fontWeight={700}
+                  mr='4'
+                  fontSize={{ base: "3xl", sm: "4xl", lg: "4xl" }}
+                  >
                     Provide the right info to the right people
-                    </Text>
-                    <Text fontSize='xl' marginRight='28' color='gray'>
-                    Some teammates don't need access to your full workflow. Permissions let you decide who sees what.
-                    </Text>
-                </Box>
-        </Box>
+                </Heading>
+                <Text fontSize={'xl'} color={"gray.500"}>
+                Some teammates don't need access to your full workflow. Permissions let you decide who sees what. 
+                </Text>
+              </Stack>
+            </Stack>
+          </Stack>
+        </Container>
         {/* ********************** box 4 ******************* */}
-        <Box height='96' w='100%' display='flex' marginTop='8'>
-                <Box p='4' height='100%'  width='58%'>
-                    <Text fontSize='4xl' marginLeft='28' marginTop='14' fontWeight='bold' marginRight='60' color='black'>
+        <Container maxW={"7xl"} mt='-12'>
+          <Stack
+            align={"center"}
+            spacing={{ base: 8, md: 10 }}
+            py={{ base: 20, md: 28 }}
+            direction={{ base: "column", md: "row" }}
+          >
+            <Stack flex={1} spacing={{ base: 5, md: 10 }}>
+              <Stack spacing={"2"} ml='20' w={"80%"}>
+                <Heading
+                  lineHeight={1.1}
+                  fontWeight={700}
+                  fontSize={{ base: "3xl", sm: "4xl", lg: "4xl" }}
+                >
                     Turn data into action
-                    </Text>
-                    <Text mt='2' fontSize='xl' ml='28' marginRight='52' color='gray'>
-                    Powered by your existing data, interfaces are an interactive layer that allow teammates to take action.
-                    </Text>
-                </Box>
-                <Box p='4' width='42%'>
-                    <Image borderRadius='12' width='99%' height='100%' margin='auto' src='https://images.ctfassets.net/wl95ljfippl8/6EuDQCRJ3v3aOX4N536v4U/6d18a34384f1ad1863c7c2ae6290e490/Tier1-InterfaceDesigner-05.png?w=1920' alt='Dan Abramov' />
-                </Box>
-        </Box>
+                </Heading>
+                <Text  mt='2' fontSize='xl' color={"gray.500"}>
+                Powered by your existing data, interfaces are an interactive layer that allow teammates to take action.
+                </Text>
+              </Stack>
+            </Stack>
+            <Flex
+              flex={1}
+              justify={"center"}
+              align={"center"}
+              position={"relative"}
+              w={"full"}
+            >
+              <Box
+                position={"relative"}
+                rounded={"2xl"}
+                width={"full"}
+              >
+                <Image
+                  alt={"Hero Image"}
+                  fit={"cover"}
+                  align={"center"}
+                  borderRadius={'24'}
+                  w={"80%"}
+                  ml='20'
+                  h={"100%"}
+                  src={
+                    "https://images.ctfassets.net/wl95ljfippl8/6EuDQCRJ3v3aOX4N536v4U/6d18a34384f1ad1863c7c2ae6290e490/Tier1-InterfaceDesigner-05.png?w=1920"
+                  }
+                />
+              </Box>
+            </Flex>
+          </Stack>
+        </Container>
         {/* ************************************************ */}
-        <Box bg='#cce0fe' marginTop='12' w='100%' p={40}>
-            <Text fontSize='5xl' marginTop='-12%' textAlign='center' fontWeight='bold' color='black'>
+        <Box bg='#cce0fe' marginTop='12' width={[
+            '150%', // 0-30em
+            '100%', // 30em-48em
+            '100%', // 48em-62em
+            '100%', // 62em+
+            ]} p={20}>
+            <Text fontSize={['md', '2xl', '4xl', '5xl']} marginTop='-12%' textAlign='center' fontWeight='bold' color='black'>
             What will you build with Airtable?
             </Text>
             <Image height='400px' width='6xl' marginTop='12' src='https://images.ctfassets.net/wl95ljfippl8/66whWGlDV24fjLHmfIvgzw/0f2911110b141203aae07c0412dc45e3/Tier1-InterfaceDesigner-01-21x9.png?w=1920' alt='Dan Abramov' />
             <Box w='100%'  display='flex' marginTop='8'>
                     <Box p='4'  width='55%'>
-                        <Text fontSize='5xl' marginLeft='-20' fontWeight='500' color='blackAlpha.700'>
+                        <Text fontSize={['md', 'xl', '4xl', '5xl']} marginLeft='-20' fontWeight='500' color='blackAlpha.700'>
                         Start building your first interface
                         </Text>
                     </Box>
                     <Box p='4'  width='45%'>
-                        <Text fontSize='xl' fontWeight='bold' marginRight='-16' marginTop='4'  color='blackAlpha.700'>
+                        <Text  fontSize={['sm', 'md', 'md', 'xl']} fontWeight='bold' marginRight='-16' marginTop='4'  color='blackAlpha.700'>
                         Love the idea of interfaces, but not sure where to start? We’ve got you.
                         </Text>
                     </Box>
@@ -299,3 +480,17 @@ const Interfacedesigner = () => {
 }
 
 export default Interfacedesigner
+
+
+
+
+
+
+
+
+
+
+
+
+
+
