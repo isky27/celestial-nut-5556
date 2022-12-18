e2d49331b672eb7321130630d610b836314675c9
isky27 <gaganjindal50@gmail.com>
2022-12-18 19:49:42 +0530

    changes

---
+++ backend/src/config/db.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+const url = process.env.DATABASE_URL;
+const connect = () => {
+    mongoose.set('strictQuery', false)
+  mongoose
+    .connect(`${url}`)
+   
+    .then(() => console.log("mongo connected"))
+    .catch((e) => console.log(e));
+};
+module.exports = connect;
